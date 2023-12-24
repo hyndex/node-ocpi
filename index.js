2cb8e7e5aeb1483d10dea113f084355342e522bc
diki bhuyan <dikibhuyan@gmail.com>
2023-12-24 14:16:15 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-// index.js
 const Location = require('./src/models/Location');
 const EVSE = require('./src/models/EVSE');
 const Connector = require('./src/models/Connector');
 const CDR = require('./src/models/CDR');
+const Command = require('./src/models/Command');
+const Feedback = require('./src/models/Feedback');
+const Meter = require('./src/models/Meter');
+const Reservation = require('./src/models/Reservation');
+const Tariff = require('./src/models/Tariff');
+const User = require('./src/models/User');
+const Transaction = require('./src/models/Transaction');
 
-module.exports = { Location, EVSE, Connector, CDR };
+module.exports = { Location, EVSE, Connector, CDR, Connector, Command, Feedback, Meter, Reservation, Tariff, Transaction, User };
